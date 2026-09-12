762a44490515ab34c7bdadb9f8a2b5e53356423d
Sunakshi027 <sunakshibkt98@gmail.com>
2026-09-12 16:50:14 +0530

    Update backend CORS

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { Server } = require("socket.io");
+
 const connectDB = require("./config/connectDB");
 const authRoutes = require("./Routes/authroute");
 const userRoutes = require("./Routes/userRoute");
@@ -11,64 +12,60 @@ const chatSocket = require("./socket/chatSocket");
 
 dotenv.config();
 
-
+// Connect MongoDB
 connectDB();
 
 const app = express();
 
+// Create HTTP server
 const server = http.createServer(app);
 
+// Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://webchat-main-1.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   },
 });
 
-
+// CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://webchat-main-1.onrender.com",
     credentials: true,
   })
 );
 
+// Middleware
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+// Test route
 app.get("/", (req, res) => {
   res.json({
     message: "WebChat backend is running",
   });
 });
 
+// API Routes
+app.use("/api/auth", authRoutes);
 
-app.use(
-  "/api/auth",
-  authRoutes
-);
+app.use("/api/users", userRoutes);
 
-app.use(
-  "/api/users",
-  userRoutes
-);
+app.use("/api/messages", messageRoutes);
 
-app.use(
-  "/api/messages",
-  messageRoutes
-);
-
-
-
+// Socket connection
 chatSocket(io);
 
-
-
+// Server Port
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
