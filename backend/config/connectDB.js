25ff25ded1b9cc8c27b290a1631d721cf4cd6f91
Sunakshi027 <sunakshibkt98@gmail.com>
2026-09-13 15:47:30 +0530

    Separate WebChat MongoDB database

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URL);
+    const connection = await mongoose.connect(
+      `${process.env.MONGO_URL}/WebChat`
+    );
 
     console.log("MongoDB connected successfully");
+    console.log("Database: WebChat");
     console.log("Host:", connection.connection.host);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
@@ -13,6 +16,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-
